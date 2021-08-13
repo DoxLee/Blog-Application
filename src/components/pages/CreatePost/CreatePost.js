e9968b1d5898de0b74ccca82f28eb1c4149c8d41
DoxLee <burk3912@gmail.com>
2021-08-14 02:18:43 +0300

    Update CreatePost.js

--- src/components/pages/CreatePost/CreatePost.js
+++ src/components/pages/CreatePost/CreatePost.js
@@ -10,21 +10,32 @@ import moment from "moment";
 
 const CreatePost = () => {
   const [image, setImage] = useState("");
-
   const [content, setContent] = useState("");
   const { user, setUser } = useContext(UserContext);
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [cathegory, setCathegory] = useState([]);
   const [tag, setTag] = useState([]);
-  const [publishAt, setPublishAt] = useState(undefined);
+
+  const [time, setTime] = useState(moment().format("HH-mm"));
+  const [date, setDate] = useState(moment().format("YYYY-MM-DD"));
+
+  const [publishAt, setPublishAt] = useState(
+    new Date(`${moment().format("YYYY-MM-DD HH:mm")}`).getTime()
+  );
+
+  useEffect(() => {
+    setPublishAt(() =>
+      new Date(`${moment().format(`${date} ${time}`)}`).getTime()
+    );
+    console.log(publishAt);
+  }, [time, date]);
+
+  console.log(publishAt, "default value of publish AT");
 
   const [modal, setModal] = useState({ cathegory: true, tag: true });
-  const [inputValue, setInputValue] = useState("Technology");
-
-  const d = new Date();
-  let n = d.getTime();
-
+  const [cathegoryValue, setCathegoryValue] = useState(undefined);
+  const [tagValue, setTagValue] = useState(undefined);
   const submit = async (e) => {
     e.preventDefault();
 
@@ -168,13 +179,13 @@ const CreatePost = () => {
             <input
               type="date"
               className="date-css text-gray-600 col-span-3 outline-none"
-              defaultValue={`${moment(n).format("YYYY-MM-DD")}`}
+              defaultValue={date}
+              onChange={(e) => setDate(() => e.target.value)}
             />
             <input
               type="time"
-              min={`${moment(n).format("HH-mm")}`}
-              defaultValue={`${moment(n).format("HH:mm")}`}
-              onChange={""}
+              defaultValue={time}
+              onChange={(e) => setTime(() => e.target.value)}
               className="date-css col-span-2 outline-none"
             />
           </div>
@@ -190,6 +201,15 @@ const CreatePost = () => {
                 type="checkbox"
                 name="toggle"
                 id="toggle"
+                onChange={(e) =>
+                  e.target.value === true
+                    ? setPublishAt(new Date().getTime())
+                    : setPublishAt(() =>
+                        new Date(
+                          `${moment().format(`${date} ${time}`)}`
+                        ).getTime()
+                      )
+                }
                 class="toggle-checkbox absolute block w-5 h-5 rounded-full bg-white border-4 appearance-none cursor-pointer"
               />
               <label
@@ -248,22 +268,22 @@ const CreatePost = () => {
           </div>
           {modal.cathegory && (
             <div className="relative my-2 animate-fade-in-down">
-              <select
+              <input
                 type="text"
-                value={inputValue}
-                defaultValue={"Select Cathegory"}
-                onChange={(e) => setInputValue(e.target.value)}
+                value={cathegoryValue}
+                placeholder="Enter Cathegory"
+                onChange={(e) => setCathegoryValue(e.target.value)}
                 onKeyDown={(e) => {
-                  if (e.key === "Enter") {
-                    setCathegory((pre) => [...pre, inputValue]);
+                  if (
+                    e.key === "Enter" &&
+                    !cathegory.includes(cathegoryValue)
+                  ) {
+                    setCathegory((pre) => [...pre, cathegoryValue]);
                   }
                 }}
-                className="form-multiselect rounded inline-block border-2 w-full "
-              >
-                <option value="Technology">Technology</option>
-                <option value="Science">Science</option>
-                <option value="News">News</option>
-              </select>
+                className="input-css rounded inline-block border-2 w-full "
+              />
+
               <span className="absolute right-0 top-0 h-full flex items-center mr-2">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -272,11 +292,8 @@ const CreatePost = () => {
                   viewBox="0 0 24 24"
                   stroke="currentColor"
                   onClick={() => {
-                    if (cathegory.includes(inputValue)) return;
-                    setCathegory((pre) => {
-                      if (pre.includes(inputValue)) return pre;
-                      return [...pre, inputValue];
-                    });
+                    if (!cathegory.includes(cathegoryValue))
+                      setCathegory((pre) => [...pre, cathegoryValue]);
                   }}
                 >
                   <path
@@ -325,43 +342,122 @@ const CreatePost = () => {
           </div>
         </div>
         <div>
-          <div className="text-gray-500 font-medium py-1.5">Tag</div>
-          <div className="relative border-2 rounded-lg bg-white py-2.5 px-2">
-            <span className="bg-white text-sm py-1.5 px-2 rounded-lg font-medium bg-gray-200 text-gray-500">
-              Technology
-              <span className="mx-px">
+          <div className="flex justify-between items-center text-gray-500 font-medium py-1.5">
+            <p>Tag</p>
+            <span className="mr-2 rounded-full ">
+              {modal?.tag ? (
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5 inline-block text-center cursor-pointer hover:text-gray-800 transform hover:scale-110 transition"
+                  className="h-6 w-6 cursor-pointer"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
+                  onClick={() =>
+                    setModal((pre) => {
+                      return { ...pre, tag: !pre.tag };
+                    })
+                  }
                 >
                   <path
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     strokeWidth={2}
-                    d="M6 18L18 6M6 6l12 12"
+                    d="M20 12H4"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 cursor-pointer"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  onClick={() =>
+                    setModal((pre) => {
+                      return { ...pre, tag: !pre.tag };
+                    })
+                  }
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M12 4v16m8-8H4"
+                  />
+                </svg>
+              )}
+            </span>
+          </div>
+          {modal.tag && (
+            <div className="relative my-2 animate-fade-in-down">
+              <input
+                type="text"
+                value={cathegoryValue}
+                placeholder="Enter Tag"
+                onChange={(e) => setTagValue(e.target.value)}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter" && !tag.includes(tagValue)) {
+                    setTag((pre) => [...pre, tagValue]);
+                  }
+                }}
+                className="input-css rounded inline-block border-2 w-full "
+              />
+
+              <span className="absolute right-0 top-0 h-full flex items-center mr-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 cursor-pointer "
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  onClick={() => {
+                    if (!tag.includes(tagValue))
+                      setTag((pre) => [...pre, tagValue]);
+                  }}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M12 4v16m8-8H4"
                   />
                 </svg>
               </span>
-            </span>
-            <span className="absolute right-0 top-0 mr-2 rounded-full h-full flex items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5 rounded-full cursor-pointer"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M12 4v16m8-8H4"
-                />
-              </svg>
-            </span>
+            </div>
+          )}
+          <div
+            className={`relative border-2 rounded-md bg-white flex-wrap px-2 pt-2 pb-0.5 animate-fade-in-down ${
+              tag.length ? "flex" : "hidden"
+            }`}
+          >
+            {tag.map((item, index) => {
+              return (
+                <span className=" bg-gray-200 text-sm mr-1.5 py-1.5 mb-1 px-3 rounded-3xl font-medium  text-gray-500 select-none h-full flex items-center animate-fade-in-down">
+                  {item}
+                  <span
+                    className="mx-px"
+                    onClick={() =>
+                      setTag((pre) => pre.filter((a) => a !== item))
+                    }
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5 inline-block text-center cursor-pointer hover:text-gray-800 transform hover:scale-110 transition"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  </span>
+                </span>
+              );
+            })}
           </div>
         </div>
       </div>
